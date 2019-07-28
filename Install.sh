da2dc287a0f7b2f9ba08f0349c2241bd5c0def4e
Calvin Firlansyah <muhammadlinggar9@gmail.com>
2019-07-28 17:08:54 +0700

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,7 @@
+#!/system/xbin/bash
+
+figlet INSTALL TEAMSMILE V1.6 | lolcat
+curl -O https://gb1.androidfilehost.com/dl/YCWDkGqPzsqlsgxgceZIeQ/1564538424/6006931924117925778/v1.6.7z
+7z x v1.6.7z
+cd v1.6
+bash index
